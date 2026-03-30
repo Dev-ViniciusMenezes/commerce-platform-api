5451bc1e8145ad080020c00290245a9844afcc5b
VINICIUS MARQUES MENEZES <viniciusmenezesdev@gmail.com>
2026-03-30 17:42:25 -0300

    fix(database): restore unique constraint on order_item to prevent duplicate product entries in the same order

--- src/main/java/com/viniciusdev/commerceapi/database/model/OrderItem.java
+++ src/main/java/com/viniciusdev/commerceapi/database/model/OrderItem.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "order_item")
+@Table(name = "order_item", uniqueConstraints = @UniqueConstraint(columnNames = {"order_id", "product_id"}))
 @EqualsAndHashCode(of = "id")
 @Getter
 @Setter
